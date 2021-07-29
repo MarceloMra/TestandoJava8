2fe7bd469b5ffb7e6fc2712332438a4c694d5fc2
MarceloMra <moreira.marcelo321@gmail.com>
2021-07-29 02:40:06 -0300

    commit inicial

---
+++ src/main/java/com/example/demo/TreinandoJava8Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinandoJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinandoJava8Application.class, args);
+	}
+
+}
